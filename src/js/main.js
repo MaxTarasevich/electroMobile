4ca35b33bda002f764ed23caf27c931f04d8b8b2
Max <maxtarasevich@gmail.com>
2021-06-20 22:26:19 +0300

    add custom select add js validation for test-drive-form

--- src/js/main.js
+++ src/js/main.js
@@ -19,8 +19,6 @@ class FormValidator {
         if(invalid != null){
           e.preventDefault()
         }
-         
-  
           self.fields.forEach(field => {
           const input = document.querySelector(`#${field}`)
           self.validateFields(input)
@@ -32,13 +30,13 @@ class FormValidator {
       let self = this
       this.fields.forEach(field => {
         const input = document.querySelector(`#${field}`)
-        
         input.addEventListener('input', event => {
           self.validateFields(input)
         })
+        input.addEventListener('click', event => {
+          self.validateFields(input)
+        })
       })
-  
-      
     }
     
     validateFields(field) {
@@ -59,7 +57,7 @@ class FormValidator {
         if (re.test(field.value)) {
           this.setStatus(field, null, "success")
         } else {
-          this.setStatus(field, "Введите телефона в формате +375112223344", "error")
+          this.setStatus(field, "Телефон в формате +375112223344", "error")
         }
       }
   
@@ -74,6 +72,23 @@ class FormValidator {
           customCheckbox.classList.add(`invalid`)
         }
       }
+
+       // check for a valid select
+     if(field.firstElementChild != null && field.firstElementChild.tagName ===`SELECT`){
+      const customSelectItems = document.querySelectorAll(`#${field.id} div`)
+      customSelectItems.forEach((item)=>{
+       item.addEventListener(`click`,()=>{
+        field.classList.remove(`error`)
+        field.classList.remove(`invalid`)
+       })
+      })
+     if(field.firstElementChild.value == 0){
+      field.classList.add(`error`)
+     } else {
+      field.classList.remove(`error`)
+      field.classList.remove(`invalid`)
+     }
+    }
      
     }
   
@@ -107,7 +122,97 @@ class FormValidator {
   }
   
   const form = document.querySelector('#form')
-  const fields = ["telephone","name","message","checkbox"]
-  
-  const validator = new FormValidator(form, fields)
-  validator.initialize()
+  if(form != null){
+    const fields = ["telephone","name","message","checkbox"]
+    const validator = new FormValidator(form, fields)
+    validator.initialize()
+  }
+  const testDriveForm = document.querySelector(`#form-test-drive`)
+  if(testDriveForm != null){
+    const fields = ["name-test-drive","telephone-test-drive","checkbox-test-drive", "select-date", "select-time"]
+    const validator = new FormValidator(testDriveForm, fields)
+    validator.initialize()
+  }
+ 
+
+/**************Custom Select**************************/
+var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect); 
